70ecf000e7f13a2b12afd73c20d27f5935c56ed1
zamworkqa <zamworkqa@gmail.com>
2023-02-14 14:11:50 +0300

    first commit

---
+++ src/test/java/com/aqa/AppTest.java
@@ -0,0 +1,19 @@
+package com.aqa;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest{
+    @Test
+    public void testlogin1(){
+        App myapp = new App();
+        Assert.assertEquals(0,myapp.userLogin("Marathon","Password"));
+    }
+
+    @Test
+    public void testlogin2(){
+        App myapp = new App();
+        Assert.assertEquals(1,myapp.userLogin("Marathon","Passw0rd"));
+    }
+
+}
---
+++ src/test/java/com/aqa/TestLogin.java
@@ -0,0 +1,27 @@
+package com.aqa;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestLogin {
+    @Test
+    public void login_Test(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+
+        Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+        driver.close();
+
+    }
+}
